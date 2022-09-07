decdb8119ba2dacac5be155b161a7f7090cf37b7
Kozlov Andrey <112641761+dramaturgerzn@users.noreply.github.com>
2022-09-07 03:42:54 +0300

    zadaniya 1-3
    test

--- client.py
+++ client.py
@@ -3,14 +3,18 @@ from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+host = 'localhost'
+port = 9090
+sock.connect((host, port))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
+print(f'Successful connection. Host: {host}, port: {port}')
+while True:
+	msg = input()
+	if msg == 'exit':
+		break
+	sock.send(msg.encode())
+	#data = sock.recv(1024)
 
 sock.close()
-
-print(data.decode())
+print('Connection lost.')
+#print(data.decode())
--- server.py
+++ server.py
@@ -2,19 +2,23 @@ import socket
 
 sock = socket.socket()
 sock.bind(('', 9090))
-sock.listen(0)
+sock.listen(1)
 conn, addr = sock.accept()
-print(addr)
-
-msg = ''
+print(f'Connecton is set. Addres is: {addr}')
 
 while True:
 	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
+		#if not data:
+		#break
+	if data:
+		msg = data.decode()
+		#conn.send(data)
+		print(msg)
+	else:
+		print('Connection lost. Waiting for connection...')
+		sock.listen(1)
+		conn, addr = sock.accept()
+		print(f'Connecton is set. Addres is: {addr}')
 
 conn.close()
+

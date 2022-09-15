e3a7529c5c9f51c731c3f4fec13d06bd354556cc
Kozlov Andrey <112641761+dramaturgerzn@users.noreply.github.com>
2022-09-15 14:39:11 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -3,11 +3,32 @@ from time import sleep
 
 sock = socket.socket()
 sock.setblocking(1)
-host = 'localhost'
-port = 9090
-sock.connect((host, port))
-
-print(f'Successful connection. Host: {host}, port: {port}')
+connected = 0
+while connected == 0:
+	addr = ['localhost', 9090]
+	port_is_correct = 0
+	try:
+		host = input('Enter the host name, default value: localhost ')
+		while port_is_correct == 0:
+			try:
+				port = input('Enter the port number, default value: 9090 ')
+				if not port:
+					port_is_correct = 1
+					break
+				port = int(port)
+				port_is_correct = 1
+			except ValueError:
+				print('Invalid port number. Enter an INTEGER.')
+		if host:
+			addr[0] = host
+		if port:
+			addr[1] = port
+		addr = tuple(addr)
+		sock.connect(addr)
+		connected = 1
+	except socket.gaierror:
+		print('Invalid data entered, try again.')
+print(f'Successful connection. Host: {addr[0]}, port: {addr[1]}')
 while True:
 	msg = input()
 	if msg == 'exit':
